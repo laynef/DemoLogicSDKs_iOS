8d69ba201ff874b2c7fdd20c4dabdf9383d609cc
lfaler <laynefaler@gmail.com>
2016-07-01 19:09:26 -0700

    Initial commit

---
+++ DemoLoginsSDKs/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  DemoLoginsSDKs
+//
+//  Created by Layne Faler on 7/1/16.
+//  Copyright © 2016 Layne Faler. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    // MARK: - Login View Controller Oulets
+    @IBOutlet weak var loginTitleLabel: UILabel!
+    @IBOutlet weak var loginUsernameTextfield: UITextField!
+    @IBOutlet weak var loginPasswordTextfield: UITextField!
+    @IBOutlet weak var loginEnterButton: UIButton!
+    @IBOutlet weak var loginSignUpButton: UIButton!
+    @IBOutlet weak var loginFacebookButton: UIButton!
+    @IBOutlet weak var loginTwitterButton: UIButton!
+    @IBOutlet weak var loginGoogleButton: UIButton!
+    @IBOutlet weak var loginLinkdInButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DemoLoginsSDKs/LogoutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LogoutViewController.swift
+//  DemoLoginsSDKs
+//
+//  Created by Layne Faler on 7/1/16.
+//  Copyright © 2016 Layne Faler. All rights reserved.
+//
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
